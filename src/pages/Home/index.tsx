6d18ff9f1eeaac0c957b09b916d9a71b3311d712
Bruno Hubner <brunohubnerdev@gmail.com>
2022-02-07 17:20:48 -0300

    feat: add pages

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles"
+
+interface Props {}
+
+export default function Home({}: Props) {
+    return (
+        <Container>
+            <h1>Home</h1>
+        </Container>
+    )
+}
---
+++ src/pages/Projects/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles"
+
+interface Props {}
+
+export default function Projects({}: Props) {
+    return (
+        <Container>
+            <h1>Projects</h1>
+        </Container>
+    )
+}
