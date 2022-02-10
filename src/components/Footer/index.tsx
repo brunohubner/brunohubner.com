f2f91ba143197cce8023b7f1d28b33d3f485fe10
Bruno Hubner <brunohubnerdev@gmail.com>
2022-02-10 14:58:50 -0300

    feat: create footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,7 @@ interface Props {}
 export function Footer({}: Props) {
     return (
         <div className="Footer">
-            <h1>Footer</h1>
+            <span>&copy; Copyright 2022 - Bruno Hubner</span>
         </div>
     )
 }
