89c02bad3f2a4f326eeabed8998f26b5f6f98f51
Bruno Hubner <brunohubnerdev@gmail.com>
2022-02-10 19:30:25 -0300

    feat: create project id page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./styles/global.scss"
 import { Projects } from "./pages/Projects"
+import { ProjectId } from "./pages/ProjectId"
 
 export function App() {
     return (
         <>
-            <Projects></Projects>
+            {/* <Projects></Projects> */}
+            <ProjectId
+                name="Nome do Projeto"
+                description="Uma breve descrição sobre este projeto que está sendo apresentado"
+                content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam autem magni possimus aut saepe delectus vel, aliquam voluptates provident alias porro voluptatum natus! Eaque ullam numquam aut quia, omnis vel. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam autem magni possimus aut saepe delectus vel, aliquam voluptates provident alias porro voluptatum natus! Eaque ullam numquam aut quia, omnis vel.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam autem magni possimus aut saepe delectus vel, aliquam voluptates provident alias porro voluptatum natus! Eaque ullam numquam aut quia, omnis vel."
+            />
         </>
     )
 }
---
+++ src/pages/ProjectId/index.tsx
@@ -0,0 +1,61 @@
+import "./styles.scss"
+import { Header } from "../../components/Header"
+import { Footer } from "../../components/Footer"
+import { Button } from "../../components/Button"
+import imageExample from "../../temp/example.png"
+import colors from "../../styles/colors"
+
+interface Props {
+    name: string
+    description: string
+    content: string
+    thumbnail?: string
+    extraImages?: string[]
+    repositoryLink?: string
+    productionLink?: string
+}
+
+export function ProjectId({
+    content,
+    description,
+    extraImages,
+    name,
+    thumbnail,
+    productionLink,
+    repositoryLink
+}: Props) {
+    return (
+        <div className="ProjectId">
+            <Header />
+            <main className="main">
+                <div className="presentation">
+                    <h1>{name}</h1>
+                    <span>{description}</span>
+                    <div className="links">
+                        <Button
+                            label="Código fonte"
+                            bgColor={colors.primaryDark}
+                        />
+                        <Button
+                            label="Link do Projeto"
+                            color={colors.black}
+                            bgColor={colors.white}
+                        />
+                    </div>
+                </div>
+                <section className="content">
+                    <div className="thumbnail">
+                        <img src={imageExample} alt={name} />
+                    </div>
+                    <p>{content}</p>
+                    {/* <div className="extraImages">
+                        {extraImages.map(image => (
+                            <img src={image} alt={name}></img>
+                        ))}
+                    </div> */}
+                </section>
+            </main>
+            <Footer />
+        </div>
+    )
+}
