62037442d3b43ccf4d7e0c373345a39e5fe6fb37
Bruno Hubner <brunohubnerdev@gmail.com>
2022-02-11 13:58:50 -0300

    feat: create home page

---
+++ src/pages/Home/components/ProfileAvatar/index.tsx
@@ -0,0 +1,9 @@
+import "./styles.scss"
+
+export function ProfileAvatar() {
+    return (
+        <div className="ProfileAvatar">
+            <h1>ProfileAvatar</h1>
+        </div>
+    )
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.scss"
+import { Terminal } from "../../components/Terminal"
+
+export function Home() {
+    return (
+        <div className="Home">
+            <main className="main">
+                <Terminal />
+            </main>
+        </div>
+    )
+}
