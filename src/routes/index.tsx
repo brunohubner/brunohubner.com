8aedba9fa01208ed07a45730ff0f9752789235cd
Bruno Hubner <brunohubnerdev@gmail.com>
2022-02-11 13:59:16 -0300

    feat: add home page in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Projects } from "../pages/Projects"
 import { ProjectId } from "../pages/ProjectId"
+import { Home } from "../pages/Home"
 
 export function AppRoutes() {
     return (
@@ -8,6 +9,7 @@ export function AppRoutes() {
             <Routes>
                 <Route path="/projects/:url_name" element={<ProjectId />} />
                 <Route path="/projects" element={<Projects />} />
+                <Route path="/" element={<Home />} />
             </Routes>
         </BrowserRouter>
     )
