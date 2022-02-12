6b61edece1fd1af1c01e1c75e57967b293cf6f8a
Bruno Hubner <brunohubnerdev@gmail.com>
2022-02-12 11:01:49 -0300

    feat: activate message box

--- src/components/Messenger/index.tsx
+++ src/components/Messenger/index.tsx
@@ -1,19 +1,43 @@
 import "./styles.scss"
 import { Terminal } from "../Terminal"
 import { SendButton } from "../SendButton"
-import { TextareaHTMLAttributes } from "react"
+import { TextareaHTMLAttributes, useState } from "react"
 
 interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {
     title: string
 }
 
 export function Messenger({ title, ...rest }: Props) {
+    const [message, setMessage] = useState("")
+    const [sended, setSended] = useState(false)
+
+    const handleMessage = (message: string) => {
+        if (message.length > 3000) return
+        setMessage(message)
+    }
+
+    const handleSendMessage = () => {
+        console.log(message)
+        setMessage("")
+        setSended(true)
+    }
+
+    if (sended) rest.placeholder = "Mensagem enviada para Bruno"
+
     return (
         <div className="Messenger">
             <Terminal title={title}>
-                <textarea className="textarea" {...rest} />
+                <textarea
+                    value={message}
+                    onChange={e => handleMessage(e.target.value)}
+                    className="textarea"
+                    {...rest}
+                />
                 <div className="sendButton">
-                    <SendButton onClick={() => console.log("enviando...")} />
+                    <SendButton
+                        disabled={message.trim().length < 1}
+                        onClick={handleSendMessage}
+                    />
                 </div>
             </Terminal>
         </div>
--- src/components/SendButton/index.tsx
+++ src/components/SendButton/index.tsx
@@ -1,13 +1,19 @@
 import "./styles.scss"
 import { IoSendSharp } from "react-icons/io5"
+import { ButtonHTMLAttributes } from "react"
 
-interface Props {
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
     onClick(): void
 }
 
-export function SendButton({ onClick }: Props) {
+export function SendButton({ onClick, ...rest }: Props) {
     return (
-        <button type="button" onClick={() => onClick()} className="SendButton">
+        <button
+            type="button"
+            onClick={() => onClick()}
+            className="SendButton"
+            {...rest}
+        >
             <IoSendSharp size={30} />
         </button>
     )
