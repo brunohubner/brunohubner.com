36c2490e2835f6e2ab91d0b7b097e7676859d128
Bruno Hubner <brunohubnerdev@gmail.com>
2022-02-13 14:34:09 -0300

    fix: projects provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./styles/global.scss"
 import { ToggleMenuProvider } from "./context/ToggleMenuContext"
 import { AppRoutes } from "./routes"
+import { ProjectsProvider } from "./context/ProjectsContext"
 
 export function App() {
     return (
-        <ToggleMenuProvider>
-            <AppRoutes />
-        </ToggleMenuProvider>
+        <ProjectsProvider>
+            <ToggleMenuProvider>
+                <AppRoutes />
+            </ToggleMenuProvider>
+        </ProjectsProvider>
     )
 }
