a5aef2c1e70592e9856cad0a788e9fea9a595690
Bruno Hubner <brunohubnerdev@gmail.com>
2023-05-15 17:47:23 -0300

    chore: linting files

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    arrowParens: "always",
-    trailingComma: "none",
-    printWidth: 80,
-    tabWidth: 2,
-    semi: true,
-    singleQuote: true,
-    endOfLine: "lf"
-}
+  arrowParens: 'always',
+  trailingComma: 'none',
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  endOfLine: 'lf'
+};
